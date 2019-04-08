2970697ce37a80ee984b6cc6e9136ba40f496e7e
Pablo Andrés Díaz <pabloandresd@gmail.com>
2019-04-08 06:35:58 -0500

    Adding some colors to the console logs

--- Program.cs
+++ Program.cs
@@ -15,12 +15,18 @@ namespace RoadSimulation
 
         private static void LogInfoToConsole(string info)
         {
+            var currentColor = System.Console.ForegroundColor;
+            System.Console.ForegroundColor = System.ConsoleColor.DarkYellow;
             System.Console.WriteLine(info);
+            System.Console.ForegroundColor = currentColor;
         }
 
         private static void LogErrorToConsole(string error)
         {
+            var currentColor = System.Console.ForegroundColor;
+            System.Console.ForegroundColor = System.ConsoleColor.Red;
             System.Console.WriteLine(error);
+            System.Console.ForegroundColor = currentColor;
         }
     }
 }
